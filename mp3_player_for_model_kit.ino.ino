171554bca2c778b89b30bc69010d743e7072571a
gratingBoy <48827595+gratingBoy@users.noreply.github.com>
2019-03-25 22:04:28 +0900

    random play bug fix
    1.add definition of serial communication.
    2.and use the DFPlayer's class library.
    3.TX,RX pins change to D10,D11.

--- mp3_player_for_model_kit.ino.ino
+++ mp3_player_for_model_kit.ino.ino
@@ -8,12 +8,26 @@
 int playFlag = 0;     //再生中フラグ
 int inPutVal;         //CDSセルからの入力値
 int playCount = 0;    //明るさフラグ
+int numfile = 0;
+
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
 
 void setup () {
+  mySoftwareSerial.begin(9600);
   Serial.begin (9600);
-  mp3_set_serial (Serial);      //set Serial for DFPlayer-mini mp3 module
-  delay(1);                      // delay 1ms to set volume
-  mp3_set_volume (20);           // value 0~30
+
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card or USB drive!"));
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }  
+  delay(1);                        // delay 1ms to set volume
+  myDFPlayer.volume(20);           // value 0~30
+  numfile = myDFPlayer.readFileCounts(DFPLAYER_DEVICE_SD);
 }
 
 void loop () {
@@ -23,7 +37,7 @@ void loop () {
   if (inPutVal > BRIGHTNESS ) {   // 暗いとき
     if (playFlag == 0) {
       if (playCount == 0) {       //周辺が暗いままなら再生しない
-        mp3_random_play ();       //再生
+        myDFPlayer.play(random(numfile)+1);       //再生
       }
       playFlag = 1;               //再生中フラグON
       playCount = 1;              //再生回数をカウントアップ
@@ -31,7 +45,8 @@ void loop () {
     }
     else {
       if (playCount == 0) {
-        mp3_stop();             //再生ストップ
+        //mp3_stop();             //再生ストップ
+        myDFPlayer.stop();
       }
       playFlag = 0;             //再生中フラグOFF
       delay(500);
